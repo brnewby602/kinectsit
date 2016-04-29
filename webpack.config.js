077b6aa33274b6aa9ea325790f7d1c88775d3b87
Bucko <bucko.perley@gmail.com>
2016-04-29 16:51:17 -0700

    (feat) Setup basic webpack config file and required packages

--- webpack.config.js
+++ webpack.config.js
@@ -41,34 +41,4 @@ module.exports = {
     path: __dirname.concat('/client/build'),
     publicPath: '/client/',
   },
-  watch: true,
-  module: {
-    preLoaders: [
-      {
-        test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/,
-      },
-    ],
-    loaders: [
-      {
-        test: /\.es6$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015'],
-        },
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
-      },
-    ],
-  },
-  plugins: [
-    new ExtractTextPlugin('styles.css', {
-      allChunks: true,
-    }),
-  ],
-  resolve: {
-    extensions: ['', '.js', '.es6'],
-  },
 };
